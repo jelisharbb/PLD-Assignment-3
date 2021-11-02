db14657d4756895c86af368dc6ecdda74dea33c1
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-02 19:30:29 +0800

    Defined the three functions

---
+++ program-1.py
@@ -0,0 +1,14 @@
+# define first the three functions
+
+def user_name():
+    name = input("What is your name? ")
+    return name
+
+def user_age():
+    age = input("How old are you? ")
+    return age
+
+def user_adress():
+    adress = input("Where do you live? ")
+    return adress
+
