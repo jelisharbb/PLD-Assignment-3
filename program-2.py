3ca8cfca87beab9f41af7a38880ff67f771efd40
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-03 19:31:26 +0800

    Code completed.

--- program-2.py
+++ program-2.py
@@ -1,7 +1,7 @@
 print("\nWelcome to Fruit Market! \nHere is the pricelist: \nApple = 20 \nOrange = 25")
 
 def apple_quantity():
-    apple = int(input("How many apples do you want to buy? "))
+    apple = int(input("\nHow many apples do you want to buy? "))
     return apple
 
 def orange_quantity():
@@ -16,3 +16,13 @@ def orange_price_per_piece():
     orange_price = 25
     return orange_price
 
+def total_price():
+    apple = (apple_quantity() * apple_price_per_piece())
+    orange = (orange_quantity() * orange_price_per_piece())
+    overall_price = (apple + orange)
+    receipt(overall_price)
+
+def receipt(overall_price):
+    print(f"\nThe total amount is {overall_price:,.2f} PHP.\n")
+
+total_price()
