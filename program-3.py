bb2ed1540bc683ec02dc43c9d5ac3c42095799c6
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-03 20:24:16 +0800

    Added else function.

--- program-3.py
+++ program-3.py
@@ -6,3 +6,7 @@ def user_change (money, apple):
         apple_max = money // apple
         print(f"You can buy {apple_max} apple/s and your change is {change:,.2f}")
 
+    else:
+        money_short = apple - money
+        print (f"Sorry, you do not have enough money to buy an apple. You need {money_short:.2f} PHP more.")
+
