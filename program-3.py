3455fc706f2e453fdaa1550bd92417da335c06eb
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-03 20:45:18 +0800

    Code completed.

--- program-3.py
+++ program-3.py
@@ -4,9 +4,13 @@ def user_change (money, apple):
     if money >= apple:
         change = money % apple
         apple_max = money // apple
-        print(f"You can buy {apple_max} apple/s and your change is {change:,.2f}")
+        print(f"\nYou can buy {apple_max:.0f} apple/s and your change is {change:,.2f} PHP. \nThank you! \n")
 
     else:
         money_short = apple - money
-        print (f"Sorry, you do not have enough money to buy an apple. You need {money_short:.2f} PHP more.")
+        print (f"\nSorry, you do not have enough money to buy an apple. You need {money_short:.2f} PHP more.")
 
+money = float(input("\nHow much money do you have? "))
+apple = int(input("How much does one apple cost? "))
+
+user_change (money, apple)
