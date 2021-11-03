ca973951d78ffbb5c6682c0b9c732d83e6cbea6b
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-03 13:25:19 +0800

    Program 1 completed

--- program-1.py
+++ program-1.py
@@ -8,7 +8,18 @@ def user_age():
     age = input("How old are you? ")
     return age
 
-def user_adress():
-    adress = input("Where do you live? ")
-    return adress
+def user_address():
+    address = input("Where do you live? ")
+    return address
 
+def complete_details():
+    userName = user_name()
+    userAge = user_age()
+    userAddress = user_address()
+    completeDetails (userName, userAge, userAddress)
+
+def completeDetails(username_, userage_, useraddress_):
+    print(f"Hi, my name is {username_}. I am {userage_} years old. I live in {useraddress_}.")
+
+
+complete_details() 
