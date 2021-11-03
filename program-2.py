c238af39efbc33559f491e6546caf1e5fb20c8f1
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2021-11-03 18:48:36 +0800

    Defined apple and orange functions and their prices.

---
+++ program-2.py
@@ -0,0 +1,18 @@
+print("\nWelcome to Fruit Market! \nHere is the pricelist: \nApple = 20 \nOrange = 25")
+
+def apple_quantity():
+    apple = int(input("How many apples do you want to buy? "))
+    return apple
+
+def orange_quantity():
+    orange = int(input("How many oranges do you want to buy? "))
+    return orange
+
+def apple_price_per_piece():
+    apple_price = 20
+    return apple_price
+
+def orange_price_per_piece():
+    orange_price = 25
+    return orange_price
+
